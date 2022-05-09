c2ce63a3e38b49669f68737592ffe13a13ac421e
diagoibrahima <elibrahimadiago@gmail.com>
2022-05-09 11:38:54 +0000

    changement lien  commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,85 @@ import { NgModule } from '@angular/core';
 import { DashboardPageComponent } from './pages/dashboard/containers';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import {AuthGuard} from './pages/auth/guards';
+import { FinancesComponent } from './pages/finances/finances.component';
+import { AvancementDetailleComponent } from './pages/avancement-detaille/avancement-detaille.component';
+import { GestionChatingComponent } from './pages/gestion-chating/gestion-chating.component';
+import { GestiondesalertesComponent } from './pages/gestiondesalertes/gestiondesalertes.component';
+import { IssueLogComponent } from './pages/issue-log/issue-log.component';
+import { MediathequeComponent } from './pages/mediatheque/mediatheque.component';
+import { MessageriesComponent } from './pages/messageries/messageries.component';
+import { ParametrageComponent } from './pages/parametrage/parametrage.component';
+import { ReferentielComponent } from './pages/referentiel/referentiel.component';
+import { SecuriteComponent } from './pages/securite/securite.component';
+import { StatistiquesComponent } from './pages/statistiques/statistiques.component';
 
 const routes: Routes = [
+  {
+    path: 'avancement-detaille',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    component: AvancementDetailleComponent
+  },
+  {
+    path: 'gestion-chating',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    component: GestionChatingComponent
+  },
+  {
+    path: 'gestiondesalertes',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    component: GestiondesalertesComponent
+  },
+  {
+    path: 'issue-log',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    component: IssueLogComponent
+  },
+  {
+    path: 'mediatheque',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    component: MediathequeComponent
+  },
+  {
+    path: 'messageries',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    component: MessageriesComponent
+  },
+  {
+    path: 'parametrage',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    component: ParametrageComponent
+  },
+  {
+    path: 'referentiel',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    component: ReferentielComponent
+  },
+  {
+    path: 'securite',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    component: SecuriteComponent
+  },
+  {
+    path: 'statistiques',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    component: StatistiquesComponent
+  },
+  {
+    path: 'finances',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+    component: FinancesComponent
+  },
   {
     path: 'dashboard',
     pathMatch: 'full',
