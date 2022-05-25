32303224d802e3a3475133bea4bf6bab44ac271e
dmgaye96 <doudoumohametgaye@gmail.com>
2022-05-25 13:38:40 +0000

    update finace compenent

--- src/app/pages/finances/finances.component.ts
+++ src/app/pages/finances/finances.component.ts
@@ -1,5 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+ interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: ' Projets phares',
+    children: [{name: 'Projet 1'}, {name: 'Projet 2'}, {name: 'Projet 3'}],
+  },
+  {
+    name: 'Réformes phares',
+    children: [{name: 'Projet 1'}, {name: 'Projet 2'}, {name: 'Projet 3'}],
+  },
+  ,
+];
+/** Flat node with expandable and level information */
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
 
 
 
@@ -8,12 +38,42 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
   templateUrl: './finances.component.html',
   styleUrls: ['./finances.component.scss']
 })
-export class FinancesComponent implements OnInit {
+export class FinancesComponent   {
   
+  private _transformer = (node: FoodNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  };
 
-  constructor() { }
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level,
+    node => node.expandable,
+  );
 
-  ngOnInit(): void {
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children,
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+
+
+  constructor() {
+    
+    this.dataSource.data = TREE_DATA;
   }
+
+
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+  
+ 
+
 
 }
