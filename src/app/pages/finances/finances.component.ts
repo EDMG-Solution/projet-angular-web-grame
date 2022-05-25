fa597dab53e98b57fb792a501038aceea8a74b2b
dmgaye96 <doudoumohametgaye@gmail.com>
2022-05-25 15:06:46 +0000

    update finace compenent

--- src/app/pages/finances/finances.component.ts
+++ src/app/pages/finances/finances.component.ts
@@ -115,14 +115,14 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Projet1', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Projet2', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Projet3', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Projet4', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Projet5', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Projet6', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Projet7', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Projet8', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Projet9', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Projet10', weight: 20.1797, symbol: 'Ne'},
 ];
